963896494082286ef2dc44d5b4c9995db77e6679
Felipe <felipe.cipolato@accurate.com.br>
2023-09-18 13:12:01 -0300

    [FEATURE][SWAGGER] - add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('CRUD Item')
+    .setDescription(
+      'The Item CRUD API is an interface that allows you to create, read, update, and delete items.',
+    )
+    .setVersion('1.0')
+    .addTag('users')
+    .addTag('auth')
+    .addTag('items')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,12 +2,31 @@ import { Body, Controller, Post } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from './users.service';
 import { User } from './schema/users.model';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('/signup')
+  @ApiOperation({
+    summary: 'Create user.',
+    description: 'Create user with name and password.',
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      example: {
+        username: 'username_example',
+        password: 'password_example',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+  })
   async createUser(
     @Body('username') username: string,
     @Body('password') password: string,
